7d853968631b4f8e48c2d76d7cf89923283eb1dc
Abel Trotter and Nate Cottle <abel@>
2018-05-30 13:27:20 -0700

    Add css, js, refactor js for add functionality in calculator

--- js/scripts.js
+++ js/scripts.js
@@ -20,5 +20,13 @@ function getNumbers() {
   alert(multiply(number1, number2));
 }
 
-//script start
-getNumbers();
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
